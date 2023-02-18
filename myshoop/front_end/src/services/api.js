e0c56473397de37d881593627433e15658d56749
EduardoVSR1995 <eduardvitor7@gmail.com>
2023-02-17 21:39:16 -0300

    fix: display settings

--- myshoop/front_end/src/services/api.js
+++ myshoop/front_end/src/services/api.js
@@ -4,18 +4,15 @@ export async function store() {
 const store = await (await fetch("/api/store", { method: "GET" })).json();
 return store;
 }
+export async function conect( url, body ) {
+return await (await fetch("/api"+shopName+url, body)).json();
+}
 
 //export async function store() {
 //  const store = await (await fetch("http://localhost:4000/store", { method: "GET" })).json();
 //  console.log(store);
 //  return store;
 //}
-  
-export async function conect( url, body ) {
-return await (await fetch("/api"+shopName+url, body)).json();
-}
-
 //export async function conect( url, body ) {   
 //  return await (await fetch("http://localhost:4000"+shopName+url, body)).json();
 //}
-
