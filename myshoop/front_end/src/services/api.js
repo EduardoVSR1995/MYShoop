187dcb96ffcda26e0c3d4eac9f541af706615ce1
EduardoVSR1995 <eduardvitor7@gmail.com>
2023-02-18 01:06:45 -0300

    fix: display settings

--- myshoop/front_end/src/services/api.js
+++ myshoop/front_end/src/services/api.js
@@ -4,6 +4,7 @@ export async function store() {
   const store = await (await fetch("/api/store", { method: "GET" })).json();
   return store;
 }
+
 export async function conect( url, body ) {
   return await (await fetch("/api"+shopName+url, body)).json();
 }
